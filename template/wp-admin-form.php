f2c153a40adc3c8aefced1f43f77a5171fce0290
the-beta-core <shoutatrikjanssen@gmail.com>
2019-11-04 23:38:01 +0100

    go

--- template/wp-admin-form.php
+++ template/wp-admin-form.php
@@ -72,9 +72,94 @@
 </div>
 
 
-<div class="bcSANY_janitor_footer">
-	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top" class="bcSANY_donate"><input name="cmd" type="hidden" value="_s-xclick"><input name="hosted_button_id" type="hidden" value="MBLCTW6UE6L5E"><input title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" name="submit" src="https://www.paypalobjects.com/en_US/NL/i/btn/btn_donateCC_LG.gif" type="image"><img src="https://www.paypal.com/en_NL/i/scr/pixel.gif" alt="" width="1" height="1" border="0"></form>
-	<a href="https://beta-media.com/super-simple-site-offline-wordpress-plugin/"><img src="<?php echo plugin_dir_url( __DIR__ ); ?>img/betalogo-b.png" /></a>
-	<h2>Check out my other plugins at</h2>
-	<p><a href="https://www.betacore.tech" target="_blank">www.betacore.tech</a></p>
+<?php 
+
+/* ------------------------ */
+/* THE FOOTER.              */
+
+$bcALG_my_plugins = array(
+                array(
+                    'slug'=>'super-simple-site-offline-beta',
+                    'name'=>'Super Simple Site Offline',
+                    'content'=>'Site offline plugins are made awesome again with this piece of code. While most site offline plugins are bulky, intrusive and annoying this one is as light as a feather and has no paid options. The nav item is neatly tucked away within the settings menu so it feels like it is part of WordPress.' ),
+                array(
+                    'slug'=>'simple-analytics-tag-beta',
+                    'name'=>'Simple Analytics Tag',
+                    'content'=>'Simple Analytics Tag helps you get up and running quick. This plugin has a non-intrusive interface and fits very well within the Wordpress Settings menu. Just paste in the ID from Google Tagmanager or Google Analytics and you are good to go.' ),
+                array(
+                    'slug'=>'super-simple-age-gate-beta',
+                    'name'=>'Super Simple Age Gate',
+                    'content'=>"Do you have to filter out younger visitors? With this super simple age gate you'll fix those age restrictions quickly. Ment for webshops and other types of websites that has to have a curtain where people below your set age can't peek behind.." )/*,
+                array(
+                    'slug'=>'super-simple-schema-markup-beta',
+                    'name'=>'Super Simple Schema Markup',
+                    'content'=>'Grab those rich snippets with this Schema plugin that adds Json-LD schema to your Wordpress website. And if you want custom schema per page that is possible as well.' ),
+                array(
+                    'slug'=>'the-janitor-beta',
+                    'name'=>'The Janitor',
+                    'content'=>'Helping you maintain that time-consuming website and in the maintime cleaning up and branding your WP-admin panel.' )*/
+            
+            );
+
+// get the slug of this plugin
+$get_slug = explode('/', plugin_basename( __FILE__ ));
+?>
+<div class="bcALG_footer">
+    <h2>Making Wordpress more awesome <span>with useful plugins like these...</span></h2>
+    
+    <ul class="bcALG_plugins">
+        <?php foreach($bcALG_my_plugins as $bc_id => $bc_value){ 
+            if($get_slug[0] != $bc_value['slug']){
+        ?>
+            <li>
+                <img src="https://ps.w.org/<?php echo $bc_value['slug']; ?>/assets/icon-256x256.jpg" title="<?php echo $bc_value['name']; ?> by Beta" class="bcALG_icon" />
+                <h3><a href="https://wordpress.org/plugins/super-simple-site-offline-beta/" target="_blank"><?php echo $bc_value['name']; ?></a></h3>
+                <p><?php echo $bc_value['content']; ?></p>
+                <a href="https://wordpress.org/plugins/<?php echo $bc_value['slug']; ?>/" class="button" target="_blank"><?php _e('Plugin page'); ?></a>
+                <a href="<?php bloginfo('wpurl'); ?>/wp-admin/plugin-install.php?tab=plugin-information&plugin=<?php echo $bc_value['slug']; ?>&TB_iframe=false" class="button button-primary" target="_blank"><?php _e('Install'); ?></a>
+            </li>
+    
+        <?php }} ?>
+    </ul>
+<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+	/* Add your own Mailchimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+    <div class="bcALG_mailinglist">
+        <form action="https://oneweekendwebsite.us20.list-manage.com/subscribe/post?u=72e22e9c5e66e05351f6c92af&amp;id=87b9e508b0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+            <h2>Are you running Wordpress inefficient? <span>Betacore is developing plugins to fix that!</span></h2>
+            <p>Get an email when new plugins arrive! The only thing you'll have to do is subscribe to the mailing list now!</p>
+            <ul class="bcALG_mailingform">
+                <li>
+                    
+			
+					<input type="text" value="" name="FNAME" class="" id="mce-FNAME" required>
+					<label for="mce-FNAME">First Name</label>
+                </li>
+                <li>
+                    
+                    
+					
+					<input type="text" value="" name="EMAIL" class="required email" id="mce-EMAIL" required/>
+					<label for="mce-EMAIL">Email Address</label>
+                </li>
+                <li>
+					<input type="submit" value="Join!" name="subscribe" id="mc-embedded-subscribe" />
+                </li>
+				
+
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_72e22e9c5e66e05351f6c92af_87b9e508b0" tabindex="-1" value=""></div>
+
+
+            </ul>
+        </form>
+    </div>
+	<div class="bcALG_logobar">
+    <a href="https://beta-media.com/super-simple-site-gate-wordpress-plugin/"><img src="<?php echo plugin_dir_url( __DIR__ ); ?>img/betalogo-b.png" /></a>
+    <p class="bcALG_url"><span>By:</span> <a href="https://www.betacore.tech" target="_blank">www.betacore.tech</a></p>
+	</div>
 </div>
+
